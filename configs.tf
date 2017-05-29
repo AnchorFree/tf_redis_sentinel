095c4a3dc9b27429fd3166844215c64c9eee0403
Yaroslav Molochko <y.molochko@anchorfree.com>
2017-05-29 21:48:19 +0300

    Dynamic quorum value

--- configs.tf
+++ configs.tf
@@ -20,6 +20,7 @@ data "template_file" "redis_sentinel_config" {
 
   vars {
     cluster_name      = "${var.cluster_name}"
+    quorum            = "${var.count / 2 + 1 }"
     master_pass       = "${var.master_pass}"
     master_ip_address = "${coalesce("${var.master_ip}", "${element(values(var.nodes), 0)}")}"
   }
