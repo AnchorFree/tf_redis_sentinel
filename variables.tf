6b8e908ceb630fbf1cd684f8b863ab223748643d
Ilya Trushchenko <i@whitehat.com.ua>
2017-08-09 12:10:48 -0700

    add todo

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "cluster_name" {
   description = "Name of the redis cluster, must be unique"
 }
 
+# TODO: remove this default after we mirgate to new DO account
 variable "do_account" {
   default     = "partner"
   description = "Preferred account to create DO instances in, default is: partner"
